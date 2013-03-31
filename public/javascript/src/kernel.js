b688265585c8b098b96fb81cd26eacbf2aa33d74
Julien Muetton <themouette@gmail.com>
2013-03-31 23:39:44 +0200

    Activate routes and canvas in kernel.

--- public/javascript/src/kernel.js
+++ public/javascript/src/kernel.js
@@ -1,3 +1,27 @@
-define(function () {
-    console.log('this is the kernel calling');
+define([
+    'vendor/jquery',
+    'vendor/underscore',
+    'vendor/backbone',
+    'app/view/canvas',
+    'app/experiences/router'
+], function ($, _, Backbone, Canvas, Experiences) {
+
+    // register module routes
+    new Experiences();
+    var router = new Backbone.Router({
+        routes: {
+            '': function () {
+                this.navigate('!/experiences', {replace: false, trigger: true});
+            }
+        }
+    });
+    Backbone.on('app:navigate', _.bind(router.navigate, router));
+
+    // initialize canvas
+    var canvas = new Canvas();
+
+    $(function () {
+        canvas.setElement($('body')).render();
+        Backbone.history.start();
+    });
 });
--- readme.markdown
+++ readme.markdown
@@ -8,4 +8,16 @@ foundation](http://foundation.zurb.com/),
 [lodash](http://lodash.com/) as [underscorejs](http://underscorejs.org/)
 replacement.
 
+Application events
+------------------
 
+* `ui:canvas:navigate`: select menu item. Following arguments are expected:
+    * `$el`: the element to insert
+    * `empty`: if `false` then the canvas won't be empty.
+* `ui:canvas:content`: replace main content. Following arguments are expected:
+    * `selector`: the selector of items to select.
+
+* `app:navigate`: navigate for a url to another. Folowing arguments are
+  expected:
+    * `url`: the url hash to navigate to.
+    * `options`: same as `Backbone.Router.navigate`.
