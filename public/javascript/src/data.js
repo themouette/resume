ffeb914a99a950f4a25aab620d5ad79cca481b53
Julien Muetton <themouette@gmail.com>
2013-04-01 01:48:52 +0200

    Some resume data.

---
+++ public/javascript/src/data.js
@@ -0,0 +1,83 @@
+define({
+    experiences: [{
+        id: "carpe-hora",
+        title: "Head and co-founder",
+        company: "Carpe Hora",
+        from: "2005-11-01",
+        until: "2012-09-30",
+        mission: "Develop an online booking system.",
+        skills: ["php", "symfony1", "propel", "postgresql", "javascript", "jquery", "backbone", "git", "puppet"]
+    }, {
+        id: "universite-d-auvergne",
+        title: "PHP teacher",
+        company: "Université d'Auvergne",
+        from: "2013-01-15",
+        until: "2013-02-15",
+        mission: "20 teaching hours to licence students",
+        skills: ["php", "phpunit"]
+    }, {
+        id: "itnetwork",
+        title: "Web developer",
+        company: "Inter Trade Network",
+        from: "2012-10-01",
+        until: "2013-03-31",
+        mission: "Develop a car retailer intranet",
+        skills: ["php", "symfony2", "doctrine", "phpunit", "mysql", "javascript", "backbone", "requirejs", "casperjs", "git"]
+    }, {
+        id: "orange-r-d",
+        title: "Gadget/Widget developer",
+        company: "Orange R&D",
+        from: "2008-06-15",
+        until: "2007-10-01",
+        mission: "Develop portlets and factorize developments",
+        skills: ["javascript", "Windows Sidebar", "Apple Dashboard", "iGoogle", "Netvibes", "eclipse", "ant"]
+    }, {
+        id: "efixo",
+        title: "Lead developer",
+        company: "Efixo",
+        from: "2007-09-30",
+        until: "2007-04-01",
+        mission: "Set up a dynamic UI for ",
+        skills: ["javascript", "Prototype", "Scriptaculous", "xslt", "C", "Linux (embedded system)"]
+    }, {
+        id: "asco-joucomatic",
+        title: "Internship",
+        company: "Asco Joucomatic",
+        from: "2004-06-01",
+        until: "2004-08-31",
+        mission: "Abroad internship",
+        skills: []
+    }, {
+        id: "msd",
+        title: "A&IT Aprentice",
+        company: "MSD",
+        from: "2002-11-15",
+        until: "2005-11-15",
+        mission: "",
+        skills: ["documentation", "Automatons", "VBA"]
+    }],
+    skills: [
+        {id: "php", title: "PHP"},
+        {id: "symfony1", title: "symfony 1.0 to 1.4"},
+        {id: "symfony2", title: "Symfony2"},
+        {id: "phpunit", title: "PhpUnit"},
+        {id: "javascript", title: "JavaScript"},
+        {id: "backbone", title: "BACKBONE.JS"},
+        {id: "requirejs", title: "RequireJs"},
+        {id: "casperjs", title: "CasperJS"},
+        {id: "git", title: "GIT"}
+    ],
+    educations: [{
+        id: "esigelec",
+        title: "Master degree in Engeniering",
+        school: "ESIGELEC",
+        from: "2003-09-01",
+        until: "2005-11-15"
+    }, {
+        id: "blaise-pascal",
+        title: "Classe préparatoire aux grandes écoles (Maths-Physics)",
+        school: "Lycée Blais Pascal",
+        from: "2002-09-01",
+        until: "2003-06-01"
+    }]
+});
