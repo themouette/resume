27e32fd6caf707006d40a6799a21da7b5c325551
Julien Muetton <themouette@gmail.com>
2013-04-18 22:55:23 +0200

    First import in gh-pages

---
+++ javascript/build/lte-ie7.js
@@ -0,0 +1,31 @@
+/*! themouette-resume - v0.0.1 - 2013-04-18 */window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-calendar' : '&#xe001;',
+			'icon-star' : '&#xe000;',
+			'icon-star-2' : '&#xe002;',
+			'icon-star-3' : '&#xe003;',
+			'icon-office' : '&#xe004;',
+			'icon-article' : '&#xe005;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
---
+++ javascript/foundation/app.js
@@ -0,0 +1,19 @@
+define([
+    'vendor/jquery',
+    'foundation/foundation',
+    'foundation/foundation.tooltips',
+    'foundation/foundation.reveal',
+    'foundation/foundation.forms',
+    'foundation/foundation.orbit',
+    'foundation/foundation.clearing',
+    'foundation/foundation.magellan',
+    'foundation/foundation.joyride',
+    'foundation/foundation.section',
+    'foundation/foundation.alerts',
+    'foundation/foundation.placeholder',
+    'foundation/foundation.cookie',
+    'foundation/foundation.topbar',
+    'foundation/foundation.dropdown'
+], function ($) {
+    $(document).foundation();
+});
